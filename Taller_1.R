03ec18825a1ac4c54f120c93690b3fbb1f81c931
juanguillermosb96 <juanguillermosb96@gmail.com>
2026-02-17 21:47:11 -0500

    Update Taller_1.R

--- Taller_1.R
+++ Taller_1.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(rvest)
 library(dplyr)
 library(skimr)
+library(stargazer)
+library(boot)
 
 #3.1 - Webscraping directo de la fuente de la pagina en todos los chunks
 urls <- paste0("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_", 1:10, ".html")
@@ -17,7 +19,7 @@ base_geih <- purrr::map_dfr(urls, ~
 #"Obrero o empleado de empresa particular"
 #"Obrero o empleado del gobierno"
 #"Empleado doméstico"
-base_geih_clean <- base_geih %>% filter(age >= 18, relab %in% c(1,2,3))
+base_geih_clean <- base_geih %>% filter(age >= 18) #, relab %in% c(1,2,3)
 
 #3.2 Data Cleaning
 # Estructura general de las variables
@@ -25,11 +27,11 @@ str(base_geih_clean)
 # Resumen de las variables
 summary(base_geih_clean)
 # para ciclo de vida (age, maxEduclevel, cotPension)
-# para brechas (sex, oficio, sizefirm, relab, totalHoursWorked)
+# para brechas (sex, oficio, sizefirm, relab)
 # para impuestos (regsalud, cotpensión, sizefirm, oficio)
 
 variables <- c("age", "maxEducLevel", "cotPension", 
-               "sex", "oficio", "sizeFirm", "relab","totalHoursWorked",
+               "sex", "oficio", "sizeFirm", "relab", "totalHoursWorked",
                "regSalud",
                "y_total_m")
 
@@ -55,7 +57,6 @@ ggplot(data = db , mapping = aes(x = oficio , y = y_total_m)) + geom_point(col =
 ggplot(data = db , mapping = aes(x = sizeFirm , y = y_total_m)) + geom_point(col = "red" , size = 0.5)
 ggplot(data = db , mapping = aes(x = relab , y = y_total_m)) + geom_point(col = "red" , size = 0.5)
 ggplot(data = db , mapping = aes(x = regSalud , y = y_total_m)) + geom_point(col = "red" , size = 0.5)
-ggplot(data = db , mapping = aes(x = totalHoursWorked , y = y_total_m)) + geom_point(col = "red" , size = 0.5)
 
 # Visualización agrupada de datos por sexo
 ggplot(data = db , mapping = aes(x = age , y = y_total_m, group = as.factor(sex), color = as.factor(sex))) + geom_point()
@@ -66,7 +67,6 @@ ggplot(data = db , mapping = aes(x = oficio , y = y_total_m, group = as.factor(s
 ggplot(data = db , mapping = aes(x = sizeFirm , y = y_total_m, group = as.factor(sex), color = as.factor(sex))) + geom_point()
 ggplot(data = db , mapping = aes(x = relab , y = y_total_m, group = as.factor(sex), color = as.factor(sex))) + geom_point()
 ggplot(data = db , mapping = aes(x = regSalud , y = y_total_m, group = as.factor(sex), color = as.factor(sex))) + geom_point()
-ggplot(data = db , mapping = aes(x = totalHoursWorked , y = y_total_m, group = as.factor(sex), color = as.factor(sex))) + geom_point()
 
 # Distribución de edad en variables
 ggplot(data=db) + geom_histogram(mapping = aes(x=y_total_m , group=as.factor(age),fill=as.factor(age)))
@@ -76,7 +76,6 @@ ggplot(data=db) + geom_histogram(mapping = aes(x=sex , group=as.factor(age),fill
 ggplot(data=db) + geom_histogram(mapping = aes(x=oficio , group=as.factor(age),fill=as.factor(age)))
 ggplot(data=db) + geom_histogram(mapping = aes(x=sizeFirm , group=as.factor(age),fill=as.factor(age)))
 ggplot(data=db) + geom_histogram(mapping = aes(x=relab , group=as.factor(age),fill=as.factor(age)))
-ggplot(data=db) + geom_histogram(mapping = aes(x=totalHoursWorked , group=as.factor(age),fill=as.factor(age)))
 
 
 # cajas por variable
@@ -96,8 +95,6 @@ ggplot(data=db , mapping = aes(as.factor(relab), y_total_m)) + geom_boxplot() + 
   scale_color_manual(values = c("0"="red" , "1"="blue"),label = c("0"="Hombre" , "1"="Mujer"),name = "Sexo")
 ggplot(data=db , mapping = aes(as.factor(regSalud), y_total_m)) + geom_boxplot() + geom_point(aes(colour=as.factor(sex))) + 
   scale_color_manual(values = c("0"="red" , "1"="blue"),label = c("0"="Hombre" , "1"="Mujer"),name = "Sexo")
-ggplot(data=db , mapping = aes(as.factor(totalHoursWorked), y_total_m)) + geom_boxplot() + geom_point(aes(colour=as.factor(sex))) + 
-  scale_color_manual(values = c("0"="red" , "1"="blue"),label = c("0"="Hombre" , "1"="Mujer"),name = "Sexo")
 
 # Valores faltantes
 
@@ -109,4 +106,106 @@ ggplot(db_miss, aes(x = reorder(skim_variable, +p_missing) , y =  p_missing)) +
   geom_bar(stat = "identity", fill = "skyblue", color = "black") +
   coord_flip() +
   labs(title = "N Missing Per Variable", x = "Var Name", y = "Missings")+ 
-  theme(axis.text = element_text(size = 5))  
+  theme(axis.text = element_text(size = 5))
+
+# 4. Sección 1
+
+# Modelo Ingreso - Edad (NO condicionado)
+mod_1 <- lm(log(y_total_m) ~ age + I(age^2), data = db)
+stargazer(mod_1,type="text", omit.stat=c("ser","f","adj.rsq"))
+
+str(mod_1)
+mod_1$coefficients
+summary(mod_1)
+
+# Edad pico (cuando la derivada del ingreso con respecto a la edad es igual a 0)
+
+# Bootstrap
+
+set.seed(1996)
+B <- 1000
+
+eta_mod_1<-rep(NA,B) #vector vacio para guardar el resultado
+length(eta_mod_1)
+
+for(i in 1:B){
+  db_sample<- sample_frac(db,size=1,replace=TRUE)
+  f<-lm(log(y_total_m) ~ age + I(age^2), data = db_sample)
+  eta_mod_1[i]<- -(f$coefficients[2])/(2*f$coefficients[3]) #saves it in the above vector
+}
+
+eta_mod_1
+plot(hist(eta_mod_1))
+mean(eta_mod_1)
+sqrt(var(eta_mod_1))
+quantile(eta_mod_1,c(0.025,0.975))
+
+# Modelo ingreso - edad - horas - relación
+
+mod_2 <- lm(log(y_total_m) ~ age + I(age^2) + totalHoursWorked + factor(relab) , data = db)
+stargazer(mod_2,type="text", omit.stat=c("ser","f","adj.rsq"))
+
+str(mod_2)
+mod_2$coefficients
+summary(mod_2)
+
+# Bootstrap
+
+set.seed(1996)
+B <- 1000
+
+eta_mod_2<-rep(NA,B) #vector vacio para guardar el resultado
+length(eta_mod_2)
+
+for(i in 1:B){
+  db_sample<- sample_frac(db,size=1,replace=TRUE)
+  f<-lm(log(y_total_m) ~ age + I(age^2) + totalHoursWorked + factor(relab), data = db_sample)
+  eta_mod_2[i]<- -(f$coefficients[2])/(2*f$coefficients[3]) #saves it in the above vector
+}
+
+eta_mod_2
+plot(hist(eta_mod_2))
+mean(eta_mod_2)
+sqrt(var(eta_mod_2))
+quantile(eta_mod_2,c(0.025,0.975))
+
+# Tabla comparativa
+
+stargazer(mod_1, mod_2,
+          type = "text",
+          add.lines = list(
+            c("Edad Pico", round(mean(eta_mod_1),2), round(mean(eta_mod_2),2)),
+            c("IC Bootstrap (2.5%)", round(quantile(eta_mod_1,c(0.025,0.975))[1],2), round(quantile(eta_mod_1,c(0.025,0.975))[2],2)),
+            c("IC Bootstrap (97.5%)", round(quantile(eta_mod_2,c(0.025,0.975))[1],2), round(quantile(eta_mod_2,c(0.025,0.975))[2],2)),
+            c("Controles de Empleo", "No", "Sí")
+          ),
+          notes = "Fuente: GEIH 2018 Bogotá.",
+          notes.align = "l")
+
+# Visualización perfiles predichos
+
+edades <- 18:80
+moda_relab <- as.numeric(names(which.max(table(db$relab))))
+
+df_preds <- data.frame(
+  age = edades,totalHoursWorked = mean(db$totalHoursWorked, na.rm = TRUE),relab = moda_relab)
+
+df_preds <- df_preds %>%
+  mutate(
+    pred_mod_1 = predict(mod_1, newdata = df_preds),
+    pred_mod_2 = predict(mod_2, newdata = df_preds)
+  )
+
+ggplot(df_preds, aes(x = age)) +
+  # Mod_1 (Incondicional)
+  geom_line(aes(y = pred_mod_1, color = "Incondicional (Solo Edad)"), size = 1) +
+  # Mod_2 (Condicional)
+  geom_line(aes(y = pred_mod_2, color = "Condicional (Horas + Relab)"), size = 1, linetype = "dashed") +
+  
+  labs(title = "Perfil Edad-Ingreso: Modelos Incondicional vs. Condicionado",
+       x = "Edad", 
+       y = "Log(Ingreso Mensual)",
+       color = "Modelo") +
+  theme_minimal() +
+  scale_color_manual(values = c("Incondicional (Solo Edad)" = "blue", 
+                                "Condicional (Horas + Relab)" = "red"))
